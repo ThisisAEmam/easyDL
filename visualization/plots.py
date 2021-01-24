403036c418d4cfca54bcfe4ca5a8d0af28396702
abdelrhmankadry <abdokadry00@gmail.com>
2021-01-24 18:07:48 +0200

    Update Model.py

--- visualization/plots.py
+++ visualization/plots.py
@@ -6,10 +6,13 @@ import numpy as np
 def plot_losses(model):
     plt.figure(figsize=(10, 8))
     plt.xticks(np.arange(1, len(model.losses)+1, 1))
-    plt.plot(model.losses)
+    plt.plot(model.losses, label = 'train loss')
+    plt.plot(model.losses_val, label = 'validation loss')
     xmin, xmax = plt.xlim()
     ymin, ymax = plt.ylim()
     plt.axis([0, len(model.losses),ymin, ymax])
+    plt.legend()
+    plt.show()
     
     
     
@@ -17,12 +20,17 @@ def plot_losses(model):
 def plot_accurcy(model):
     plt.figure(figsize=(10, 8))
     plt.xticks(np.arange(1, len(model.acc)+1, 1))
-    plt.plot(model.acc)
+    plt.plot(model.acc, label = 'train accurcy')
+    plt.plot(model.val_acc, label = 'validation accurcy')
     xmin, xmax = plt.xlim()
     ymin, ymax = plt.ylim()
     plt.axis([0, len(model.acc),ymin, ymax])
-    
+    plt.legend()
+    plt.show()
     
 
     
     
+
+    
+
